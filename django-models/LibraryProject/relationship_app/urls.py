bb2e8b66f43f6b527184d02b793b5348c6578004
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-11-16 21:15:22 +0300

    changed the pattern in urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,26 @@ from django.urls import path
 from . import views
 from .views import list_books
 from .views import LibraryDetailView
-
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path(
+        'login/',
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+
+    # Registration (custom view)
     path('register/', views.register_view, name='register'),
+
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
+
+
+
