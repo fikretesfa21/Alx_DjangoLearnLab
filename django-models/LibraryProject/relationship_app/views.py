037f556c7aa91b7dbfbb066ebed704f910d10be0
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-11-16 21:10:37 +0300

    changed the import in view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.views.generic.detail import DetailView
 from .models import Library , Book
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
