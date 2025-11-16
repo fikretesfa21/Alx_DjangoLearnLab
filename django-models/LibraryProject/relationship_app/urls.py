812888dcd804e3a1e4bfec03b52d5df0e44aa29f
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-11-16 20:59:50 +0300

    changed the detail view template

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
