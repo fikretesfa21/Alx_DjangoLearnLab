4845b5c14fb1bc4205bb8965581613924d4b7f02
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-11-30 21:37:55 +0300

    another spacing error

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewset):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
