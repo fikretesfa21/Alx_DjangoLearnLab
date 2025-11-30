9907af90705b209b9a0a6065861e1d9771105a89
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-11-30 21:36:39 +0300

    another attempt

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,11 +2,12 @@ from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+class BookViewSet(viewsets.ModelViewset):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewset(viewsets.ModelViewset):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    
