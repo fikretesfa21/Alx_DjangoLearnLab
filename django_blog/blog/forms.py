7b3ace578d94912637252baf445ab0c30ed877a4
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-12-13 21:01:58 +0300

    changed the form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,8 +2,20 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Comment, Post
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
+    # ... (existing code)
+
+# ... (existing code for UserUpdateForm and CommentForm)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
     email = forms.EmailField()
 
     class Meta:
